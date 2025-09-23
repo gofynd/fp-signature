945853927354d0cdb09bf3e9a197a5c292d20956
vivek-gofynd <vivekprajapati@fynd.com>
2025-09-23 23:38:50 +0530

    Updated signature changes

--- src/utils.ts
+++ src/utils.ts
@@ -1,27 +1,22 @@
-import CryptoJS from 'crypto-js';
+import sha256 from "crypto-js/sha256";
+import hmacSHA256 from "crypto-js/hmac-sha256";
 
-export function hmac(key: string, string: string, encoding: 'hex' | 'base64' = 'hex'): string {
-    const hmac = CryptoJS.HmacSHA256(string, key);
-    return hmac.toString(encoding === 'hex' ? CryptoJS.enc.Hex : CryptoJS.enc.Base64);
+export function hmac(key: string, string: string, encoding: any) : string {
+    return hmacSHA256(string, key).toString();
 }
 
-export function hash(string: string, encoding: 'hex' | 'base64' = 'hex'): string {
-    const hash = CryptoJS.SHA256(string);
-    return hash.toString(encoding === 'hex' ? CryptoJS.enc.Hex : CryptoJS.enc.Base64);
+export function hash(string: string, encoding: any): string {
+    return sha256(string).toString();
 }
 
-// RFC 3986 compliant URL encoding
-export function encodeRfc3986(urlEncodedString: string): string {
+// This function assumes the string has already been percent encoded
+export function encodeRfc3986(urlEncodedString: string) : string {
     return urlEncodedString.replace(/[!'()*]/g, function (c: string) {
         return "%" + c.charCodeAt(0).toString(16).toUpperCase();
     });
 }
 
-export function encodeRfc3986Full(str: string): string {
-    if (typeof str !== 'string') {
-        throw new Error('Input must be a string');
-    }
-    
-    // Properly encode the string according to RFC 3986
-    return encodeRfc3986(encodeURIComponent(str));
+export function encodeRfc3986Full(str: string) : string{
+    return str;
+    // return encodeRfc3986(encodeURIComponent(str));
 }
